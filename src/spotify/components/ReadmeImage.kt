9c6acac94ce7d752abed7cddf59df903972c6592
Tim Kante <tim@timkante.dev>
2021-01-31 22:29:39 +0100

    :lipstick: added some css for dark mode

--- src/spotify/components/ReadmeImage.kt
+++ src/spotify/components/ReadmeImage.kt
@@ -153,5 +153,23 @@ data class ReadmeImage(
             |    transform: scaleX(1)
             |  }
             |}
+|
+            |@media (prefers-color-scheme: dark) {
+            |  #cover {
+            |    box-shadow: 0 1px 3px rgb(255,255,255,0.1), 0 3px 7px rgb(255,255,255,0.05);
+            |  }
+            |  #progress {
+            |    background-color: #e1e4e8;
+            |  }
+            |  .progress-bar {
+            |    border: 1px solid #555f69;
+            |  }
+            |  #track {
+            |    color: #e1e4e8 !important;
+            |  }
+            |  #artist {
+            |    color: #555f69 !important;
+            |  }
+            |}
             |""".trimMargin(marginPrefix = "|")
 }
